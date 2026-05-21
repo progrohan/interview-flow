596155d9f26602fa8e8bfd7d1f5fe548cf96ad3f
Andrey <aprishlov@gmail.com>
2026-05-21 18:59:12 +0300

    feat: created method for getting random questions

--- src/main/java/com/progrohan/interview_flow/repository/QuestionRepository.java
+++ src/main/java/com/progrohan/interview_flow/repository/QuestionRepository.java
@@ -2,6 +2,21 @@ package com.progrohan.interview_flow.repository;
 
 import com.progrohan.interview_flow.entity.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
+
+
+    @Query(value =
+    """
+        SELECT id
+        FROM questions
+        WHERE profession_id = :professionId
+        ORDER BY RANDOM()
+        LIMIT 7
+    """, nativeQuery = true)
+    List<Long> findRandomQuestionIds(Long professionId);
+
 }
