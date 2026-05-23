ce3a23fd17f53f898544cfcc994bcb2e05722c25
Andrey <aprishlov@gmail.com>
2026-05-23 19:47:48 +0300

    chore: added annotations for tests

--- src/main/java/com/progrohan/interview_flow/model/UserAnswer.java
+++ src/main/java/com/progrohan/interview_flow/model/UserAnswer.java
@@ -1,10 +1,16 @@
 package com.progrohan.interview_flow.model;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class UserAnswer {
 
