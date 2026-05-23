ea0b52dad53eb45f0a216870e96cddfff1d7c131
Andrey <aprishlov@gmail.com>
2026-05-24 02:54:45 +0300

    fix: make UserDetails Serializable

--- src/main/java/com/progrohan/interview_flow/model/CustomUserDetails.java
+++ src/main/java/com/progrohan/interview_flow/model/CustomUserDetails.java
@@ -2,38 +2,46 @@ package com.progrohan.interview_flow.model;
 
 import com.progrohan.interview_flow.entity.Role;
 import com.progrohan.interview_flow.entity.User;
+import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.io.Serializable;
 import java.util.Collection;
 import java.util.List;
 
-public class CustomUserDetails implements UserDetails {
+public class CustomUserDetails implements UserDetails, Serializable {
 
-    private final User user;
+    private static final long serialVersionUID = 1L;
+
+    @Getter
+    private final Long id;
+    private final String username;
+    private final String password;
+    @Getter
+    private final Role role;
 
     public CustomUserDetails(User user) {
-        this.user = user;
+        this.id = user.getId();
+        this.username = user.getName();
+        this.password = user.getPassword();
+        this.role = user.getRole();
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority( user.getRole().name()));
+        return List.of(new SimpleGrantedAuthority(role.name()));
     }
 
     @Override
     public String getPassword() {
-        return user.getPassword();
+        return password;
     }
 
     @Override
     public String getUsername() {
-        return user.getName();
-    }
-
-    public Role getRole() {
-        return user.getRole();
+        return username;
     }
 
     @Override
