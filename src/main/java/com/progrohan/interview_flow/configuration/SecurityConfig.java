a54ecdb95f821747d2ffcfe4b219ad4ba57a0302
Andrey <aprishlov@gmail.com>
2026-05-24 02:11:04 +0300

    fix: changed redis host

--- src/main/java/com/progrohan/interview_flow/configuration/SecurityConfig.java
+++ src/main/java/com/progrohan/interview_flow/configuration/SecurityConfig.java
@@ -1,14 +1,12 @@
 package com.progrohan.interview_flow.configuration;
 
-import com.progrohan.interview_flow.service.CustomUserDetailsService;
+
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
