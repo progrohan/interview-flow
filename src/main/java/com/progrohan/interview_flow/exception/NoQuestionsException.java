c72e01e30023a52adea0492229ae289986b819c6
Andrey <aprishlov@gmail.com>
2026-05-25 04:25:25 +0300

    feat: created exception for no questions

---
+++ src/main/java/com/progrohan/interview_flow/exception/NoQuestionsException.java
@@ -0,0 +1,9 @@
+package com.progrohan.interview_flow.exception;
+
+public class NoQuestionsException extends RuntimeException {
+
+    public NoQuestionsException(String message) {
+        super(message);
+    }
+
+}
